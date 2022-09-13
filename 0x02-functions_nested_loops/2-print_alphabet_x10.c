74a93efdc69de5068261ba5940a4cfd4ae53235d
Kizzyx0x0 <alokanayokunle@gmail.com>
2022-09-14 00:43:24 +0100

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * main- print x10 the alphabets
+ * 
+ * Return: Always 0 (success)
+ * 
+ */
+
+void print_alphabet_x10(void);
+{
+    int alpha;
+    int alpha2;
+
+    for (alpha = 0; alpha <= 9; alpha++)
+    {
+        for (alpha = 'a'; alpha <= 'z'; alpha++)
+        {
+            _putchar(alpha2);
+        }
+        _putchar('\n');
+    }
+    
+
+}

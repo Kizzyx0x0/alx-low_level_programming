a43ae0a1d47f5f8e6c44bbb67669fee38b136357
Kizzyx0x0 <alokanayokunle@gmail.com>
2022-09-14 01:43:27 +0100

    The World looks like a multiplication-table, or a mathematical equation, which, turn it how you will, balances itself

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,62 +1,49 @@
 #include "main.h"
+
 /**
-* three_dig - prints the series of characters if i * j is > 100
-* @n: number to analyze
-*
-* Return: nothing
-*/
-void three_dig(int n)
-{
-	_putchar(',');
-	_putchar(' ');
-	_putchar(n / 100 + '0');
-	if ((n / 10) % 10 <= 0)
-		_putchar('0');
-	else
-		_putchar((n % 100) / 10 + '0');
-	_putchar(n % 10 + '0');
-}
-/**
-* print_times_table - prints the n times table, starting with 0
-* @n: the number times table
-*
-* Return: nothing
-*/
+ * print_times_table - Print the `n` times table, starting with 0.
+ * Description: If `n` is greater than 15 or less than 0, print nothing.
+ * @n: int type number
+ */
 void print_times_table(int n)
 {
-	int i, j, product;
+	int x = 0, y, z;
 
-	if (n >= 0 && n <= 15)
+	if (n > 15 || n < 0)
+		return;
+	while (x <= n)
 	{
-		for (i = 0; i <= n; i++)
+		for (y = 0; y <= n; y++)
 		{
-			for (j = 0; j <= n; j++)
+			z = x * y;
+			if (z > 99)
 			{
-				product = i * j;
-				if (product > 99)
-				{
-					three_dig(product);
-				}
-				else if (product / 10 > 0)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(product / 10 + '0');
-					_putchar((product) % 10 + '0');
-				}
-				else if (j != 0)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(product % 10 + '0');
-				}
-				else
-					_putchar(product % 10 + '0');
+				_putchar(z / 100 + '0');
+				_putchar((z / 10 % 10) + '0');
+				_putchar(z % 10 + '0');
 			}
-			_putchar('\n');
+			else if (z > 9)
+			{
+				_putchar(' ');
+				_putchar(z / 10 + '0');
+				_putchar(z % 10 + '0');
+			}
+			else if (y != 0)
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(z + '0');
+			}
+			else
+				_putchar(z + '0');
+
+			if (y != n)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
 		}
+		_putchar('\n');
+		x++;
 	}
 }
